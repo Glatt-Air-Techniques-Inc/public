96e11620e2e750418d68bae016711a837be2f65a
djcloutier <djcloutier@gmail.com>
2023-02-03 15:06:01 -0500

    Update and rename final.sh to finalize.sh

--- gch2/finalize.sh
+++ gch2/finalize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -o /usr/bin/setup https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/startup.sh
+chmod +x /usr/bin/startup
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 curl -sSL  https://repo.45drives.com/setup -o setup-repo.sh
