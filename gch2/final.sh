3445629f836f89983609c07d3d3891b01bec95fd
djcloutier <djcloutier@gmail.com>
2023-02-03 14:59:07 -0500

    Create final.sh

---
+++ gch2/final.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+curl -sSL  https://repo.45drives.com/setup -o setup-repo.sh
+bash setup-repo.sh
+apt update
+apt install -y cockpit-navigator
