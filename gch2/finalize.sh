39f63e9e0af7e9ee5b37199962f714331cf8f211
Dave Cloutier <david.cloutier@glatt.com>
2023-02-03 17:26:26 -0500

    updated finalize

--- gch2/finalize.sh
+++ gch2/finalize.sh
@@ -4,6 +4,8 @@ virsh pool-start default
 virsh pool-autostart default
 curl -o /usr/bin/setup https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/startup.sh
 chmod +x /usr/bin/startup
+curl -o /usr/bin/gch https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/gch.sh
+chmod +x /usr/bin/gch
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 curl -sSL  https://repo.45drives.com/setup -o setup-repo.sh
