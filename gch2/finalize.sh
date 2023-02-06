d690a4ba1d4ccc1bd0efbb9bf94f76c9a7eb4858
djcloutier <djcloutier@gmail.com>
2023-02-06 15:13:25 -0500

    Update finalize.sh

--- gch2/finalize.sh
+++ gch2/finalize.sh
@@ -8,7 +8,7 @@ curl -o /usr/bin/gch https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/
 chmod +x /usr/bin/gch
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-curl -o ~/cockpit-navigator.deb https://github.com/Glatt-Air-Techniques-Inc/public/raw/master/navigator_1_amd64.deb
+curl -o ~/cockpit-navigator.deb https://github.com/Glatt-Air-Techniques-Inc/public/blob/master/navigator_1_amd64.deb?raw=true
 apt install -y cockpit-navigator.deb
 bash setup-repo.sh
 apt update
