95c11866a61ed6073b4e02ab1851a952b6d764d7
djcloutier <djcloutier@gmail.com>
2023-02-09 16:34:05 -0500

    Update finalize.sh

--- gch2/finalize.sh
+++ gch2/finalize.sh
@@ -20,3 +20,26 @@ sudo virsh pool-autostart default
 sudo adduser glatt libvirt 
 sudo adduser glatt libvirt-qemu
 sudo adduser glatt kvm
+
+echo 'Changing netplan to NetowrkManager on all interfaces'
+# backup existing yaml file
+cd /etc/netplan
+cp 01-netcfg.yaml 01-netcfg.yaml.BAK
+
+# re-write the yaml file
+cat << EOF > /etc/netplan/01-netcfg.yaml
+# This file describes the network interfaces available on your system
+# For more information, see netplan(5).
+network:
+  version: 2
+  renderer: NetworkManager
+EOF
+
+# setup netplan for NM
+netplan generate
+netplan apply
+# make sure NM is running
+systemctl enable NetworkManager.service
+systemctl restart NetworkManager.service
+
+echo 'Done!'
