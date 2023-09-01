26543cdd5961fbabfe4bc392d458d61e5fa9646c
Matthew Meyers <matthew.meyers@glatt.com>
2023-09-01 15:07:03 -0400

    formatting and file locations

--- gch2/finalize.sh
+++ gch2/finalize.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
-curl -o /usr/bin/setup https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/startup.sh
+gch2_pub_git='https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/'
+tailscale_repo='https://pkgs.tailscale.com/stable/ubuntu/'
+
+# GCH2 public repo shell gets
+curl -o /usr/bin/setup "${gch2_pub_git}startup.sh"
 chmod +x /usr/bin/setup
-curl -o /usr/bin/gch https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/gch.sh
+curl -o /usr/bin/gch "${gch2_pub_git}gch.sh"
 chmod +x /usr/bin/gch
-curl -o /usr/bin/get-vagrant https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/get-vagrant.sh
+curl -o /usr/bin/get-vagrant "${gch2_pub_git}get-vagrant.sh"
 chmod +x /usr/bin/get-vagrant
-curl -o /usr/bin/start https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/start
+curl -o /usr/bin/start "${gch2_pub_git}start"
 chmod +x /usr/bin/start
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-wget https://github.com/Glatt-Air-Techniques-Inc/public/raw/master/navigator_1_amd64.deb
+
+# Tailscale and Cockpit
+curl -fsSL "${tailscale_repo}jammy.noarmor.gpg" | \
+ sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL "${tailscale_repo}jammy.tailscale-keyring.list" | \
+ sudo tee /etc/apt/sources.list.d/tailscale.list
+wget "${gch2_pub_git}"pkg/navigator_1_amd64.deb
 sudo apt update
 sudo apt install -y genisoimage
 sudo apt install -y tailscale
 sudo apt install -y ./navigator_1_amd64.deb
+
+# Virsh
 sudo virsh pool-define-as default dir - - - - "/var/lib/libvirt/images"
 sudo virsh pool-start default
 sudo virsh pool-autostart default
 
-#update users
+# Udate users
 sudo adduser glatt libvirt 
 sudo adduser glatt libvirt-qemu
 sudo adduser glatt kvm
 
-echo 'Changing netplan to NetowrkManager on all interfaces'
+# Networking
 # backup existing yaml file
+echo 'Changing netplan to NetworkManager on all interfaces'
 cd /etc/netplan
 cp 01-netcfg.yaml 01-netcfg.yaml.BAK
-
 # re-write the yaml file
 cat << EOF > /etc/netplan/01-netcfg.yaml
 # This file describes the network interfaces available on your system
@@ -36,18 +46,17 @@ network:
   version: 2
   renderer: NetworkManager
 EOF
-
 # setup netplan for NM
 netplan generate
 netplan apply
-
 # make sure NM is running
 systemctl enable NetworkManager.service
 systemctl restart NetworkManager.service
 
-# remove message-of-the-day advertisements
+# Remove message-of-the-day advertisements
 sudo sed -i 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
-# add Please Run Setup warning
-curl https://raw.githubusercontent.com/Glatt-Air-Techniques-Inc/public/master/gch2/profile | sudo tee -a '/home/glatt/.profile'
+
+# Add 'Please Run Setup' warning
+curl "${gch2_pub_git}profile" | sudo tee -a '/home/glatt/.profile'
 
 echo 'finalize.sh Done!'
